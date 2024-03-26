370b052b8cc3b721f63b4e3a79a59b23a885f3a0
2.D Pažout Rudolf <Rudolf.Pazout@student.sps-cl.cz>
2024-03-26 15:39:28 +0100

    node

--- databáze připojení/app.js
+++ databáze připojení/app.js
@@ -1,39 +1,56 @@
-
-const mysq2 = require('mysql2');
+const mysql2 = require('mysql2');
 const express = require('express');
 const path = require('path');
- 
-const connection = mysql2.createConnection (
+const { error } = require('console');
+
+const connection = mysql2.createConnection(
     {
         host: '127.0.0.1',
         user: 'root',
         password: 'root',
-        port: '3360',
+        port: '3306',
         database: 'moje_db'
     }
 );
- 
-connection.connect((error) => {
-    if (error) {
+
+connection.connect((error) =>{
+    if (error){
         console.log(error);
     } else {
-        console.log("Úspěšně připojeno.");
+        const jmeno = "Rudolf"
+        const prijmeni = "Pazout"
+        console.log("úspěšně připojeno.")
         connection.query(
-            `INSERT INTO uzivatele(jmeno, prijmeni) VALUES('${jmeno}', '${prijmeni}');`
-        , (error, data) => {
+            `INSERT INTO uzivatele(jmeno,prijmeni) Values('${jmeno}','${prijmeni}');`
+        ,   (error,data) => {
             if (error) console.log(error);
         });
- 
         connection.query(
-            'SELECT * FROM uzivatele;', (error, data) => {
-                if (error) console.log(error);
-                else {
-                    console.log(data);
-                }
+        'SELECT * FROM uzivatele;', (error,data) => {
+            if (error) console.log(error);
+            else{
+                console.log(data);
             }
+        }
         )
     }
-});
+})
+
+const app = express();
+app.use(express.static(__dirname));
+app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+})
+
+app.post("/uloz-uzivatele", (req,res) => {
+    console.log(req.body);
+    res.redirect("/");
+})
+
+app.listen(5500);
  
 const app = express();
 app.use(express.static(_dirname));
