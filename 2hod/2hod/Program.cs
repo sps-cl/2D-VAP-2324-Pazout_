2e1832b23e71cd019cd22dced818535204f6780e
2.D Pažout Rudolf <Rudolf.Pazout@student.sps-cl.cz>
2024-04-02 16:49:48 +0200

    postup

---
+++ 2hod/2hod/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Zadej sekvenci čísel:");
+                string input = Console.ReadLine();
+                string[] numbers = input.Split(',');
+
+                int[] parsedNumbers;
+                try
+                {
+                    parsedNumbers = numbers.Select(int.Parse).ToArray();
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Některý z vstupů není celé číslo. Zadejte prosím znovu čísla a zkontroluj te si zda jsou to celé čísla.");
+                    continue;
+                }
+
+                int min = parsedNumbers.Min();
+                int max = parsedNumbers.Max();
+                int longestAscendingInterval = GetLongestAscendingInterval(parsedNumbers);
+
+                Console.WriteLine($"Nejmenší číslo je {min}.");
+                Console.WriteLine($"Největší číslo je {max}.");
+                Console.WriteLine($"Délka nejdelšího vzestupného intervalu je {longestAscendingInterval}.");
+
+                Console.WriteLine("Chcete opakovat tento proces? (ano/ne)");
+                string choice = Console.ReadLine();
+                if (choice.ToLower() != "ano")
+                    break;
+            }
+        }
+
+        static int GetLongestAscendingInterval(int[] numbers)
+        {
+            int longestInterval = 0;
+            int currentInterval = 1;
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] > numbers[i - 1])
+                    currentInterval++;
+                else
+                {
+                    if (currentInterval > longestInterval)
+                        longestInterval = currentInterval;
+                    currentInterval = 1;
+                }
+            }
+
+            if (currentInterval > longestInterval)
+                longestInterval = currentInterval;
+
+            return longestInterval;
+        }
+    }
+}
---
+++ C#/uvod/uvod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uvod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("zadek první číslo");
+            
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("zadejte druhe číslo");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine($"vysledek je:{a + b}");
+            Console.Read();
+        }
+    }
+}
